853e6caebd5ba60d9ca621506e5a4ad3cb0c5ecf
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-06-02 00:40:07 -0500

    Adding the portfolio and contact section

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,7 +94,21 @@ modalCloses.forEach((modalClose, i) => {
     })
 })
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 /*==================== TESTIMONIAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
